afcbab8e1b5b649338db6653f998f21af44a545f
Olle Brickarp <olle.brickarp@ny.se>
2018-09-12 18:36:20 +0200

    Work work

---
+++ themes/mdk/static/js/master.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  /* Wrap menu item labels and buttons */
+  $('.menu a, .btn').each(function() {
+      var letters = $(this).text().split('');
+
+      $(this).html(function() {
+        return $.map(letters, function(letter, i) {
+          return "<span class='letter'>"+letter+"</span>"
+        });
+      });
+
+      $(this).find('.letter').each(function() {
+        var letter = $(this).text();
+        if(letter == " ") {
+          $(this).addClass('letter--space');
+        }
+      });
+
+    console.log($(this).find('.letter').length);
+
+    if($(this).find('.letter').length > 13) {
+      $(this).addClass('item--long');
+    }
+  });
+
+  $('.faq__question').click(function() {
+    $(this).parent().toggleClass('faq__item--open');
+    $(this).parent().find('.faq__answer').slideToggle(200);
+  });
+});
